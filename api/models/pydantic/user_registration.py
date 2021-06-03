ab421bb62e16d75247dc48615be368f8518f8b69
Florian d'Erfurth <florian@derfurth.com>
2021-06-03 19:58:04 +0200

    wip register endpoint

---
+++ api/models/pydantic/user_registration.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+
+
+class UserRegistration(BaseModel):
+    email: str
+    firstname: str
+    lastname: str
+    password: str
--- api/routers/v2/auth.py
+++ api/routers/v2/auth.py
@@ -1,9 +1,9 @@
 import requests
 from fastapi import APIRouter
-from fastapi.responses import HTMLResponse
 from starlette.responses import JSONResponse
 
 from api.config.configuration import AUTH_KEYCLOAK, AUTH_REALM, AUTH_CLIENT_ID, AUTH_SECRET
+from api.models.pydantic.user_registration import UserRegistration
 
 router = APIRouter(prefix='/v2/auth')
 
@@ -13,17 +13,30 @@ environment_domains = {
     "local": "https://sandboxterritoires.osc-fr1.scalingo.io",
 }
 
+token_endpoint = f'{AUTH_KEYCLOAK}/auth/realms/{AUTH_REALM}/protocol/openid-connect/token'
+auth_endpoint = f'{AUTH_KEYCLOAK}/auth/realms/{AUTH_REALM}/protocol/openid-connect/auth'
 
-@router.post('/{environment}/register', response_class=HTMLResponse)
-async def register(environment: str):
-    """todo: call users API"""
-    pass
+
+@router.post('/{environment}/register', response_class=JSONResponse)
+async def register(environment: str, registration: UserRegistration):
+    """Register a new user"""
+    token_parameters = {
+        'client_id': AUTH_CLIENT_ID,
+        'client_secret': AUTH_SECRET,
+        'grant_type': 'client_credentials',
+    }
+    token_response = requests.post(token_endpoint, data=token_parameters)
+    token_json = token_response.json()
+    access_token = token_json['access_token']
+
+    headers = {'Authorization': 'Bearer ' + access_token}
+    response = requests.post(auth_endpoint, json=registration.json(), headers=headers)
+    return response.json()
 
 
 @router.get('/{environment}/token', response_class=JSONResponse)
 async def token(environment: str, code: str):
     """Returns a token from an code"""
-    token_endpoint = f'{AUTH_KEYCLOAK}/auth/realms/{AUTH_REALM}/protocol/openid-connect/token'
     parameters = {
         'client_id': AUTH_CLIENT_ID,
         'client_secret': AUTH_SECRET,
