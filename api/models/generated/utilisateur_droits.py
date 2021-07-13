ab48d60e3a82f7101a4585aa3acb8cfccaec2ab6
Florian d'Erfurth <florian@derfurth.com>
2021-07-13 09:57:15 +0200

    code regen

--- api/models/generated/utilisateur_droits.py
+++ api/models/generated/utilisateur_droits.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
 from datetime import date
-from dataclasses import dataclass
 from typing import List
+from pydantic import BaseModel
 
 
-@dataclass
-class UtilisateurDroits:
+class UtilisateurDroits(BaseModel):
     ademe_user_id: str
     epci_id: str
     ecriture: bool
